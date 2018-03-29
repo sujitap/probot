a29d26f1ad123a3a699396c73b570cd61e81bcdf
sujitap <sujitra.p@softsq.com>
2018-03-29 16:49:38 +0700

    test

--- controllers/recastcontroller.js
+++ controllers/recastcontroller.js
@@ -3,14 +3,10 @@
 const recast = require('recastai');
 const configs = require('../configs');
 
-function fnrecast(conver){
+module.exports.fnrecast = function(conver){
     // let recastrequest = new recast.request(configs.recastToken, 'en');
     var build = new recast.build(configs.recastToken, 'en');
     var CONVERSATION_ID = conver || '';
 
     build.dialog({ type: 'text', content: 'Hello Recast'}, { conversationId: CONVERSATION_ID });
-}
-
-module.exports = {
- fnrecast: fnrecast(conver)
 };
